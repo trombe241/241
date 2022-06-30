76066aa283269615c8684bb0fc6a95062fbff981
西井翔 <SHO@SHO-Air.local>
2022-06-30 15:31:30 +0900

    first commit

--- terraform.tf
+++ terraform.tf
@@ -2,12 +2,6 @@ terraform {
   required_version = "~> 1.2.4"
 
   /*
-  backend s3は検証時はコメントアウトしておくこと。
-  Terraformで作成したリソースの状態を複数のメンバーで共有するためにtfstateファイルの保管先を指定する。
-  詳細は下記参照
-   https://www.terraform.io/docs/language/settings/backends/s3.html
-
-
   backend "s3" {
     region = "ap-northeast-1"
     bucket = "tfstate-share"
